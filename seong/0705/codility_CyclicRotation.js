730d1e9da41633cced48f44f4388aa6a6e8347ce
Seong Young Jeong <70461368+GleamingStar@users.noreply.github.com>
2021-07-05 17:58:18 +0900

    Revert "[Seong]2021-07-05"

--- seong/0705/codility_CyclicRotation.js
+++
@@ -1,4 +0,0 @@
-// 런타임에러 (75%)
-// const solution = (A, K) => A.map((_, i) => A[(A.length + i - K) % A.length]);
-
-const solution = (A, K) => A.map((_, i) => A[(A.length + i - (K % A.length)) % A.length]);
